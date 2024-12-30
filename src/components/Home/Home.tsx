7afc52d19deac6f8847c72a56e0a31432b644226
Vaibhav-kalurkar <Vaibhav.kalurkar@infeonit.in>
2024-12-30 18:42:37 +0530

    Animation on home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css';
 import Homeimg from '../../assets/waste-processing-factory_118813-24576-removebg.png';
+import Aos from 'aos';
 
 const Home = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
     <section id='home'>
       <div className='container-fluid'>
         <div className='home container'>
           <div className='home-text'>
-            <h1>
-              Sunrise <strong>Cardboard Solutions</strong> for a Sustainable
+            <h1 data-aos='fade-right'>
+              Sunrise <strong>Packaging Solutions</strong> for a Sustainable
               Future
             </h1>
-            <p>
+            <p data-aos='fade-right'>
               Reliable, eco-friendly solutions for all your cardboard needs.
               Simplify your operations while promoting sustainability.
             </p>
-            <button className='btn-explore'>Explore Solutions</button>
+            <button
+              className='btn-explore'
+              data-aos='fade-right'
+            >
+              Explore Products
+            </button>
           </div>
-          <div className='image-container'>
+          <div
+            className='image-container'
+            data-aos='zoom-in-left'
+          >
             <img
               src={Homeimg}
               alt='Sustainable Solutions'
